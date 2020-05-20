c1757ee0277887fbb2d2df5bc4d206302c5a1e1b
fallenstar82 <fallenstar82@gmail.com>
2020-05-20 17:01:12 +0900

    스크립트 긴급 버그 수정.

--- run.bat
+++ run.bat
@@ -1,12 +1,14 @@
-sqlplus -S sys/Ehdgoddba!23@pdc_statdb1 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_statdb2 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_lotg1 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_lotg2 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_pyunhap1 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_pyunhap2 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_elsdb1 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_elsdb2 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_bokgwon1 as sysdba @check.sql
-sqlplus -S sys/Ehdgoddba!23@pdc_bokgwon2 as sysdba @check.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_statdb1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_statdb2 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_lotg1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_lotg2 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_pyunhap1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_pyunhap2 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_elsdb1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_elsdb2 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_bokgwon1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@pdc_bokgwon2 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@bdc_lotg1 as sysdba @dayCheck.sql
+sqlplus -S sys/Ehdgoddba!23@bdc_lotg2 as sysdba @dayCheck.sql
 
-dailyCheck.exe check_LOTG1.log check_LOTG2.log check_elsdb1.log check_elsdb2.log check_PYUNHAP1.log check_PYUNHAP2.log check_STATDB1.log check_STATDB2.log check_BOKGWON1.log check_BOKGWON2.log
+dailyCheck.exe CHECK_LOTG1_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_LOTG2_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_elsdb1_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_elsdb2_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_PYUNHAP1_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_PYUNHAP2_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_STATDB1_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_STATDB2_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log CHECK_BOKGWON1_%DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%.log
